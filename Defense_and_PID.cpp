9cee5cc2cbfb3095e837ca6a948ea5b767e70274
tejaram15 <raghuramkarra15@gmail.com>
2018-04-03 17:28:56 +0530

    Updated KeeperSoloplay.

--- Defense_and_PID.cpp
+++ Defense_and_PID.cpp
@@ -24,7 +24,6 @@ boost::system::error_code err;
 grSim_Packet send_packet;
 grSim_Robot_Command* command;
 double x_vel,y_vel,w_vel,angle,angle_between_bot_and_ball;
-//Keeper Solo Play
 bool keeper_in_position = false;
 
 
@@ -65,8 +64,15 @@ bool operator==(const SSL_DetectionRobot& p1, const SSL_DetectionRobot& p2){
 //Calculate angle from x axis to a given two point
 double calc_angle_between_points(double x1,double y1,double x2,double y2){
 	if(x2 - x1 != 0)
-		return atan2(y2-y1,x2-x1);
+		return fmod((atan2(y2-y1,x2-x1) * 180/PI)+450.0,360.0);
 	return 0;
+}
+
+double calc_angle_between_lines(double x1,double y1,double x2,double y2){
+	double angle1 = calc_angle_between_points(0.0,0.0,x1,y1);
+	double angle2 = calc_angle_between_points(0.0,0.0,x2,y2);
+	//printf("angle1:%3.2f angle2:%3.2f  ",angle1,angle2);
+	return angle1 - angle2;
 }
 
 int main(){	    
@@ -79,55 +85,8 @@ int main(){
     printf("Connecting to Multicast Server......\n");
     client.open(true);
 	//Server Correctly Connected
-	
-	//call keeper Solo Play
-	command->set_id(0);
-	if(!keeper_in_position){
-		// for yellow team
-		keeper_robot_pos.set_x(3000);
-		keeper_robot_pos.set_y(0);
-		while(!keeper_in_position){
-			detection = recieve_packet.detection();
-			ball = detection.balls(0);			
-			tracking_robot_pos = detection.robots_yellow(0);//Send robot-1 near the ball
-			//printf("Bot Position:(%3.2f,%3.2f)  ",tracking_robot_pos.x(),tracking_robot_pos.y());
-			if(tracking_robot_pos == destination_robot_pos) keeper_in_position = true;
-			angle = tracking_robot_pos.orientation() * 180/PI;
-			angle_between_bot_and_ball = calc_angle_between_points(3000,0,tracking_robot_pos.x(),tracking_robot_pos.y());
-			printf("Bot angle: %3.2f  ",angle_between_bot_and_ball);
-			if(angle >= -180.0 && angle <= -0.0) angle += 360.0; 
-			x_vel = pid_X.calculate(3000.0,tracking_robot_pos.x());
-			y_vel = pid_Y.calculate(0.0,tracking_robot_pos.y());
-			w_vel = pid_W.calculate(angle_between_bot_and_ball,angle);
-			printf("PidX: %3.2f  PidY: %3.2f  PidW: %3.2f\n",x_vel,y_vel,w_vel);
-			send_packet.mutable_commands()->set_isteamyellow(true);
-			send_packet.mutable_commands()->set_timestamp(0.0);
-			command = send_packet.mutable_commands()->add_robot_commands();
-			command->set_wheelsspeed(false); //ambiguous
-			command->set_veltangent(x_vel); //change
-			command->set_velnormal(y_vel);
-			if(abs(90.0 - angle) < 180.0)
-			command->set_velangular(-w_vel);
-			else command->set_velangular(w_vel);
-			command->set_velangular(0.0);
-			command->set_kickspeedx(0.0);
-			command->set_kickspeedz(0.0);
-			command->set_spinner(false);		
-			
-			send_packet.SerializeToOstream(&stream);
-			
-			//Send the Command
-			std::string data = stream.str();
-			//std::cout<<"str: "<<stream.str()<<"\n";	
-			socket.send_to(buffer(data,int(data.length())),remote_endpoint,0,err);			
-		}
-		printf("Exiting\n");
-	}
-
-
-	//Start fetching data from the detected packet	
-/*	bool reached = false;
-	while(!reached){
+/*
+    while(!keeper_in_position){
 		if (!client.receive(recieve_packet)){
 			printf("Connection to Server Unsuccessful!!\n");
 			return -1;
@@ -137,30 +96,30 @@ int main(){
 			printf("Recieved Packet has no Detection Frame!!\n");
 			return -1;
 		}
-		//printf("Everything's Going well buddy!!\n");
 		detection = recieve_packet.detection();
 		ball = detection.balls(0);
-		destination_robot_pos.set_x(0.0);
-		destination_robot_pos.set_y(0.0);		
+		keeper_robot_pos.set_x(3000.0);
+		keeper_robot_pos.set_y(0.0);
 		tracking_robot_pos = detection.robots_yellow(0);//Send robot-1 near the ball
 		//printf("Bot Position:(%3.2f,%3.2f)  ",tracking_robot_pos.x(),tracking_robot_pos.y());
-		if(tracking_robot_pos == destination_robot_pos) reached = true;
+		grSim_Packet send_packet;
 		send_packet.mutable_commands()->set_isteamyellow(true);
 		send_packet.mutable_commands()->set_timestamp(0.0);
-		command = send_packet.mutable_commands()->add_robot_commands();
+		grSim_Robot_Command* command = send_packet.mutable_commands()->add_robot_commands();
 		
-
+		if(tracking_robot_pos == keeper_robot_pos) keeper_in_position = true;
+		
+		command->set_id(0);
 		angle = tracking_robot_pos.orientation() * 180/PI;
-		angle_between_bot_and_ball = calc_angle_between_points(3000,0,tracking_robot_pos.x(),tracking_robot_pos.y());
-		printf("  bot angle: %3.2f  ",angle_between_bot_and_ball);
-		if(angle >= -180.0 && angle <= -0.0) angle += 360.0; 
-		x_vel = pid_X.calculate(0.0,tracking_robot_pos.x());
+		if(angle >= -180.0 && angle <= -0.0) angle += 360.0;
+		angle_between_bot_and_ball = calc_angle_between_points(3000.0,0.0,tracking_robot_pos.x(),tracking_robot_pos.y()); 
+		printf("angle:%3.2f ",angle_between_bot_and_ball);
+		x_vel = pid_X.calculate(3000.0,tracking_robot_pos.x());
 		y_vel = pid_Y.calculate(0.0,tracking_robot_pos.y());
 		w_vel = pid_W.calculate(angle_between_bot_and_ball,angle);
-		printf("PidX: %3.2f  PidY: %3.2f  PidW: %3.2f",x_vel,y_vel,w_vel);
-
-		command->set_wheelsspeed(false); //ambiguous
-		command->set_veltangent(x_vel); //change
+		printf("PidX: %3.2f  PidY: %3.2f  PidW: %3.2f ",x_vel,y_vel,w_vel);
+		command->set_wheelsspeed(false);
+		command->set_veltangent(x_vel); 
 		command->set_velnormal(y_vel);
 		if(abs(90.0 - angle) < 180.0)
 		command->set_velangular(-w_vel);
@@ -172,21 +131,63 @@ int main(){
 		
 		send_packet.SerializeToOstream(&stream);
 		
-		//Send the Command
 		std::string data = stream.str();
 		//std::cout<<"str: "<<stream.str()<<"\n";	
 		socket.send_to(buffer(data,int(data.length())),remote_endpoint,0,err);
-		
-/*		Good Old Testing
-		angle = tracking_robot_pos.orientation() * 180/PI;
-		if(angle >= -180.0 && angle <= -0.0) angle += 360.0; 
-		x_vel = pid_X.calculate(0.0,tracking_robot_pos.x());
-		y_vel = pid_Y.calculate(0.0,tracking_robot_pos.y());
-		w_vel = pid_W.calculate(360.0,angle);		
-//		printf("PidX: %3.2f  PidY: %3.2f  PidW: %3.2f",x_vel,y_vel,w_vel);
 		std::cout<<"\n";
 	}
-*/
+	printf("Keeper in Position\n");*/
+	// Keeper in Position protect the ball
+	while(true){
+		if (!client.receive(recieve_packet)){
+			printf("Connection to Server Unsuccessful!!\n");
+			return -1;
+		}
+		//printf("Client Successfully Connected\n");
+		if (!recieve_packet.has_detection()){
+			printf("Recieved Packet has no Detection Frame!!\n");
+			return -1;
+		}
+		detection = recieve_packet.detection();
+		ball = detection.balls(0);
+		keeper_robot_pos.set_x(3000.0);
+		keeper_robot_pos.set_y(0.0);
+		tracking_robot_pos = detection.robots_yellow(0);//Send robot-1 near the ball
+		//printf("Bot Position:(%3.2f,%3.2f)  ",tracking_robot_pos.x(),tracking_robot_pos.y());
+		grSim_Packet send_packet;
+		send_packet.mutable_commands()->set_isteamyellow(true);
+		send_packet.mutable_commands()->set_timestamp(0.0);
+		grSim_Robot_Command* command = send_packet.mutable_commands()->add_robot_commands();
+		
+		if(tracking_robot_pos == keeper_robot_pos) keeper_in_position = true;
+		
+		command->set_id(0);
+		angle = tracking_robot_pos.orientation() * 180/PI;
+//		printf("angle1: %3.2f ",angle);
+		angle = fmod((angle+450),360.0);
+//		printf("angle2: %3.2f\n",angle);		
+		angle_between_bot_and_ball = calc_angle_between_lines(ball.x(),ball.y(),tracking_robot_pos.x(),tracking_robot_pos.y()); 
+		printf(" angle:%3.2f ",angle_between_bot_and_ball);
+		x_vel = pid_X.calculate(3000.0,tracking_robot_pos.x());
+		y_vel = pid_Y.calculate(0.0,tracking_robot_pos.y());
+		w_vel = pid_W.calculate(angle_between_bot_and_ball,angle);
+		printf("PidX: %3.2f  PidY: %3.2f  PidW: %3.2f ",x_vel,y_vel,w_vel);
+		command->set_wheelsspeed(false);
+		command->set_veltangent(0.0); 
+		command->set_velnormal(0.0);
+		command->set_velangular(w_vel);
+		command->set_kickspeedx(0.0);
+		command->set_kickspeedz(0.0);
+		command->set_spinner(false);		
+		
+		send_packet.SerializeToOstream(&stream);
+		
+		std::string data = stream.str();
+		//std::cout<<"str: "<<stream.str()<<"\n";	
+		socket.send_to(buffer(data,int(data.length())),remote_endpoint,0,err);
+		std::cout<<"\n";
+	}
+
 	printf("\nExiting\n");
 	socket.close();
 	return 0;
